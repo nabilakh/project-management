d91fa27c984d17344da05897aa1d68762ed05a9b
nabila.kh <nabila.kh@nusantara.technology>
2025-05-18 14:26:34 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SideNavbar from "./components/SideNavbar";
+
+function App() {
+  return (
+    <>
+      <SideNavbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SideNavbar.jsx
@@ -0,0 +1,11 @@
+export default function SideNavbar() {
+  return (
+    <nav>
+      <h2>YOUR PROJECTS</h2>
+      <button>+ Add Project</button>
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  );
+}
